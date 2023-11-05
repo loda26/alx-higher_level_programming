0aaf721f39d312abec32208140908b26dd67ef0c
loda26 <mokhaled26498@gmail.com>
2023-11-05 15:07:15 -0800

    q

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,6 +2,6 @@
 def max_integer(my_list=[]):
     if len(my_list) < 1:
         return None
-    c_list = my_list.copy()
-    c_list.sort()
-    return c_list(-1)
+    list_copy = my_list.copy()
+    list_copy.sort()
+    return (list_copy[-1])
