d639f848bd271f6f2ec391b01a9c68dba597ab3e
loda26 <mokhaled26498@gmail.com>
2023-11-05 14:49:29 -0800

    q

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def def add_tuple(tuple_a=(), tuple_b=())::
+    a1 = tuble_a[0] if len(tuble_a) > 0 else 0
+    a2 = tuble_a[1] if len(tuble_a) > 1 else 0
+    b1 = tuble_a[0] if len(tuble_a) > 0 else 0
+    a2 = tuble_a[1] if len(tuble_a) > 1 else 0
+    return (a1 + b1, a2 + b2)
