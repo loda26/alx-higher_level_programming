6bcf9098d981ed3c87f703f1b4c29a0aef9597a0
loda26 <mokhaled26498@gmail.com>
2023-11-08 06:02:04 -0800

    q

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lamda submat: list(map(lamda e: e ** 2, submat)), matrix))
+    return list(map(lambda submat: list(map(lambda e: e ** 2, submat)), matrix))
