f10bc41d074a64e092dc40c535ffa576a3975f5c
loda26 <mokhaled26498@gmail.com>
2023-11-21 12:20:53 -0800

    q

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -9,6 +9,17 @@ class Square:
 
     @property
     def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')     
+        self.__size = value
+
+    def area(self):
         return self.__size ** 2
 
     def my_print(self):
