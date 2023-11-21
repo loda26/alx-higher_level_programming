487103a91a8a676d15d80767ff4c893561b4edc5
loda26 <mokhaled26498@gmail.com>
2023-11-21 10:07:53 -0800

    q

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Define a square."""
+
+    def __init__(self, size):
+        """constructor
+
+        Args:
+            size: length of a size if the square.
+        """
+        self.__size = size
