971d7dd9fbb66d3515af6e9115e106576e278ace
loda26 <mokhaled26498@gmail.com>
2023-11-21 13:37:05 -0800

    q

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,6 +12,18 @@ class Square:
     def size(self):
         return (self.__size)
 
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @property
+    def position(self):
+        return (self.__position)
+
     @position.setter
     def position(self, value):
         if (not isinstance(value, tuple) or
