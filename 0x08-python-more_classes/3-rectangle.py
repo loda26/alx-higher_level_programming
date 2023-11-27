bc1862885622b6a5b2260c6f8f24c7b3a0c90e49
loda26 <mokhaled26498@gmail.com>
2023-11-27 22:22:43 +0200

    q

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -49,7 +49,9 @@ class Rectangle:
     
     def __str__(self):
         """return printed string in form of Rec"""
-        string = ""
-        if self.__width != 0 and self.__height != 0:
-            string += "\n".join("#" * self.__width for i in range(self.__height))
-        return string
+        rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
