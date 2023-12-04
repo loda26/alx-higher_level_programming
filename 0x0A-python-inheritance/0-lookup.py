a3102f0206feea15abb6f46dc459796899f50591
loda26 <mokhaled26498@gmail.com>
2023-12-04 16:59:01 +0200

    q

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Module for lookup function"""
+"""Defines an object attribute lookup function."""
+
 
 def lookup(obj):
     """Return a list of an object's available attributes."""
