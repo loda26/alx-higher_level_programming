54349b19391ee594b0746b9e26eb8f08faab3552
loda26 <mokhaled26498@gmail.com>
2023-12-11 00:12:58 +0200

    q

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,3 +31,14 @@ class Base:
 			return "[]"
 		else:
 			return json.dumps(list_dictionaries)
+	
+	@classmethod
+	def save_to_file(cls, list_objs):
+		"""Write the JSON serialization of a list of objects to a file."""
+		filename = cls.__name__ + ".json"
+		with open(filename, "w") as jsonfile:
+			if list_objs is None:
+				jsonfile.write("[]")
+			else:
+				list_dicts = [o.to_dictionary() for o in list_objs]
+				jsonfile.write(Base.to_json_string(list_dicts))
