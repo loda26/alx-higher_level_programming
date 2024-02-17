1b547bdf5a6e43b6403b4ba366aa0858a18b6d1e
loda26 <mokhaled26498@gmail.com>
2024-02-17 16:52:28 +0200

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,16 +6,20 @@ all cities of that state, using the database
 import MySQLdb
 from sys import argv
 
-# The code should not be executed when imported
 if __name__ == '__main__':
-    # make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name FROM cities\
+    insert_stmt = "SELECT cities.id, cities.name FROM cities\
                 INNER JOIN states ON cities.state_id = states.id\
-                WHERE states.name = %s", [argv[4]])
+                WHERE states.name = %s"
+    data = [argv[4]]
+    cur.execute(insert_stmt, data)
 
     rows = cur.fetchall()
     j = []
@@ -23,6 +27,5 @@ if __name__ == '__main__':
         j.append(i[1])
     print(", ".join(j))
 
-    # Clean up process
     cur.close()
     db.close()
