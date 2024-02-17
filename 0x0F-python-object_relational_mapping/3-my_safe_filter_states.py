ea66ce23b0ef0626b5caaa86d3307861448ae906
loda26 <mokhaled26498@gmail.com>
2024-02-17 07:03:57 +0200

    task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,11 +9,15 @@ from sys import argv
 
 if __name__ == '__main__':
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
+    insert_stmt = "SELECT * FROM states WHERE BINARY name = %s", [argv[4]]
+    cur.execute(insert_stmt)
 
     rows = cur.fetchall()
     for i in rows:
