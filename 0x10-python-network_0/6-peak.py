60494df941bc9901292dc976515dc0cc1eca8973
loda26 <mokhaled26498@gmail.com>
2024-03-01 23:52:08 +0200

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -17,8 +17,8 @@
 def find_peak(list_of_integers):
     """BRUTE force implementation for question
     """
-    max_i = None
-    for ele in list_of_integers:
-        if max_i is None or max_i < ele:
-            max_i = ele
-    return max_i
+    result = None
+    for tmp in list_of_integers:
+        if result is None or result < tmp:
+            result = tmp
+    return result
