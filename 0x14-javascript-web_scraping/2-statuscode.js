36499fa9c48e01f1c0ff37d07beda77e1f17b181
loda26 <mokhaled26498@gmail.com>
2024-03-26 22:14:49 +0200

    task

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,13 +2,10 @@
 
 const request = require('request');
 const url = process.argv[2];
-
-request.get(
-    url, function (error, response) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('code: ' + response.statusCode);
-        }
-    }
-);
+request.get(url, function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:' + ' ' + response.statusCode);
+  }
+});
